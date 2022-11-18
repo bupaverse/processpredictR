8fc9e1ec530ec6f44a2abf1798b99d65befc1675
Ivan Esin <ivan.esin99@gmail.com>
2022-11-18 13:25:13 +0100

    tokenize is used inside fit_model and predict_model

--- R/fit_model.R
+++ R/fit_model.R
@@ -3,28 +3,30 @@
 #' (test)
 #'
 #' @param transformer_model A defined transformer model
-#' @param tokens_train A list of train tokens, i.e. token_x, token_y
+#' @param train_data A training dataset
 #' @param num_epochs  A number of epochs
 #' @param batch_size A batch size
 #' @param file Name of saved model (weights)
 #'
 #' @export
-fit_model <- function(transformer_model, tokens_train, num_epochs, batch_size, file) {
+fit_model <- function(transformer_model, train_data, num_epochs, batch_size, file) {
   UseMethod("fit_model")
 }
 #' @export
-fit_model.ppred_model <- function(transformer_model, tokens_train, num_epochs, batch_size, file) {
+fit_model.ppred_model <- function(transformer_model, train_data, num_epochs, batch_size, file) {
 
   # max_case_length
   maxlen <- attr(transformer_model, "max_case_length") %>% as.integer()
+  num_epochs <- num_epochs %>% as.integer()
+  batch_size <- batch_size %>% as.integer()
+
+  # tokenizing activities from the train dataset
+  tokens_train <- tokenize(train_data)
 
   # same for all tasks
   train_token_x <- tokens_train$token_x %>% keras::pad_sequences(maxlen = maxlen, value = 0)
   train_token_x <- train_token_x %>% reticulate::np_array(dtype = "float32")
   train_token_y <- tokens_train$token_y  %>% reticulate::np_array(dtype = "float32")
-
-  num_epochs <- num_epochs %>% as.integer()
-  batch_size <- batch_size %>% as.integer()
 
   # features
   if (attr(model, "num_features") > 0) {
