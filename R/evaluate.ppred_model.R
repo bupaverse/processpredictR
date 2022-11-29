e98e9621f269903844c07c9089ae82e14855a3b1
Ivan Esin <ivan.esin99@gmail.com>
2022-11-29 20:16:54 +0100

    update generic evaluate()
    - renamed
    - change arguments calls
    - if-else class examples_df or examples_tokens

---
+++ R/evaluate.ppred_model.R
@@ -0,0 +1,42 @@
+#' Generic evaluate function for ppred_model class.
+#'
+#' wip
+#'
+#' @param object [`ppred_model`] (default [`NULL`]): ProcessTransformer model of class [`ppred_model`].
+#' @param test_data [`ppred_examples_df`] (default [`NULL`]): preprocessed test data.
+#' @param ... Additional arguments
+#'
+#' @importFrom tensorflow evaluate
+
+#' @export
+evaluate.ppred_model <- function(object, test_data, ...) {
+
+  # if test_data is a preprocessed test dataset (before tokenize)
+  if (any((test_data %>% class) == "ppred_examples_df")) {
+    tokens_test <- test_data %>% tokenize()
+}
+  # if test_data is already tokenized
+  else if (any((test_data %>% class) == "ppred_examples_tokens")) {
+    tokens_test <- test_data
+  }
+
+
+  x_tokens_test <- tokens_test$token_x %>% keras::pad_sequences(maxlen = object$max_case_length, value = 0)
+  y_tokens_test <- tokens_test$token_y
+
+  # add extra features to a list of inputs
+  if (object$number_features > 0) {
+    x_features <- tokens_test$time_x
+    results <- keras::evaluate(object$model, list(x_tokens_test, x_features), y_tokens_test, ...) %>% keras::k_argmax(axis = -1) %>% as.integer()
+  }
+  else {
+    results <- keras::evaluate(object$model, x_tokens_test, y_tokens_test, ...) %>% keras::k_argmax(axis = -1) %>% as.integer()
+  }
+
+  return(results)
+}
+
+
+
+
+
