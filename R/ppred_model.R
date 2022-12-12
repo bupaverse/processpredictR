bf8b062a42e1edaa037fd40cc89fc96519c4531c
Ivan Esin <ivan.esin99@gmail.com>
2022-12-12 10:27:08 +0100

    update documentation
    - added cross-reference ppred_model, ppred_examples_df
    - added documentation
    - rbuildignore doesn't ignore vignette build and subsequently __pycache__ files

---
+++ R/ppred_model.R
@@ -0,0 +1,6 @@
+#' @title ppred_model object
+#' @name ppred_model
+#' @docType class
+#' @description object of type [`ppred_model`] is a list returned by processpredictR::create_model() containing a custom keras functional (transformer) model and some other useful metrics of an event log.
+#'
+NULL
--- R/ppred_predictions.R
+++ R/ppred_predictions.R
@@ -1,5 +1,5 @@
 #' @title ppred_predictions object
-#' @name predictions
+#' @name ppred_predictions
 #' @docType class
 #' @description object of type [`ppred_predictions`] is a data.frame with predicted values returned by predict.ppred_model().
 #'
